27b31fc875362d034e73ee959d49f755f630facc
Diego <dtellez95@gmail.com>
2023-02-25 22:05:36 +0000

    visualize convolutions and pooling

--- fundamentals_of_ML/improveComputerVision_Convolution.py
+++ fundamentals_of_ML/improveComputerVision_Convolution.py
@@ -24,6 +24,8 @@ model.fit(training_images, training_labels, validation_data=(val_images, val_lab
 model2 = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
     tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(20, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
