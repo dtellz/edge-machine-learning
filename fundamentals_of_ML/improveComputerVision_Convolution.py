409285c57a92829a3b121553d27153dcb1af4faf
Diego <dtellez95@gmail.com>
2023-02-25 10:20:28 +0000

    last exer

--- fundamentals_of_ML/improveComputerVision_Convolution.py
+++ fundamentals_of_ML/improveComputerVision_Convolution.py
@@ -8,18 +8,31 @@ mnist = tf.keras.datasets.fashion_mnist
 training_images = training_images / 255.0
 val_images = val_images / 255.0
 
-model = tf.keras.models.Sequential([
+""" model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(20, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(training_images, training_labels, validation_data=(val_images, val_labels), epochs=20)
+model.fit(training_images, training_labels, validation_data=(val_images, val_labels), epochs=20) """
 
+
+#Rebuild it to a CNN
 model2 = tf.keras.models.Sequential([
-    
+    tf.keras.layers.Conv2D(64, (3, 3,), activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3, 3,), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
+
+model2.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model2.summary()
+model2.fit(training_images, training_labels, validation_data=(val_images, val_labels), epochs=20)
+
 
 
 
